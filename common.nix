fd6090816fb2fa6d308b2e16540106ce6f848097
Bakhtiyar Neyman <b.neymanov@gmail.com>
2019-09-19 01:26:01 -0700

    Add shortcut to restart fish.

--- common.nix
+++ common.nix
@@ -181,6 +181,7 @@ in {
         interactiveShellInit = ''
           function fish_user_key_bindings
             bind \cr 'peco_select_history (commandline -b)'
+            bind \cs 'exec fish'
           end
         '';
       };
