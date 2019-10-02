1d0cc4fc96f8d2a87708b4c293abc723ef2f2db5
Bakhtiyar Neyman <bineyman@gmail.com>
2019-10-02 05:40:15 +0000

    Use nvidia on iron

--- hosts/iron.nix
+++ hosts/iron.nix
@@ -9,7 +9,11 @@
     ];
 
   networking.hostName = "iron";
-  services.xserver.videoDrivers = [ "nvidia" "intel" ];
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.optimus_prime.enable = true;
+  # Bus ids can be found using lspci.
+  hardware.nvidia.optimus_prime.nvidiaBusId = "PCI:1:0:0";
+  hardware.nvidia.optimus_prime.intelBusId = "PCI:0:2:0";
   boot.kernelPackages = pkgs.linuxPackages;
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
