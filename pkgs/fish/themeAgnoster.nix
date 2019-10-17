67349da1efb52aba25f4f163a0e47d295e6a80ed
Bakhtiyar Neyman <b.neymanov@gmail.com>
2019-10-16 23:38:02 -0700

    Update theme agnoster package

--- pkgs/fish/themeAgnoster.nix
+++ pkgs/fish/themeAgnoster.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchFromGitHub }:
 stdenv.mkDerivation rec {
   name = "theme-agnoster";
-  version = "nix-shell";
+  version = "master";
 
   src = fetchFromGitHub {
     owner = "niobium0";
     repo = name;
     rev = "${version}";
-    sha256 = "1r30byr8k39v6afxwq0vkagl93f9psidchpdn4yi4cpb9k6ca6cp";
+    sha256 = "1i0zmq9j8fhl16w90r0lwrzf6k441xpkdyg7cwygh7vavm2wndzs";
   };
 
   installPhase = import ./installPhase.nix;
